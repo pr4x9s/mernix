52701456ece58fc449034c99d30c6de7bba4f6f1
pr4x9s <9tbs6@proton.me>
2026-08-14 22:38:25 +0530

    client/refactor(register): refactor Register form inputs and fix mobile responsive layout
    - replaced all native `<input>` elements in `Register.tsx` with the reusable `Input` component
    - integrated `leftIcon` with Lucide icons with click-to-focus behavior using React Hook Form's `setFocus`
    - cleaned up inline error rendering for field-level validations
    - adjusted avatar/cover max-size hint text placement (`pl-36`) to prevent text overlapping on `320px` screen viewports
    - replaced native `<button>` element in `Register.tsx` with the reusable `Button` component
    
    Signed-off-by: `pr4x9s` <9tbs6@proton.me>

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,12 @@
 import { useState, type ChangeEvent } from 'react'
 import { useRegister } from '../hooks/useRegister.ts'
 import { useForm, type SubmitHandler } from 'react-hook-form'
-import type { RegisterData } from '../types/types.ts'
 import { Link } from 'react-router'
-import { Camera, User, Video } from 'lucide-react'
+import { AtSign, Camera, Eye, EyeOff, Loader2, Lock, Mail, User, Video } from 'lucide-react'
+import { registerUserSchema, type RegisterFormData } from '../validators/auth.validator.ts'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Input from '../components/common/Input.tsx'
+import Button from '../components/common/Button.tsx'
 
 
 
@@ -11,19 +14,22 @@ const Register = () => {
 	
 	const { mutate: registerUser, isPending } = useRegister();
 
-	const [avatarFile, setAvatarFile] = useState<File | null>(null);
-	const [coverFile, setCoverFile] = useState<File | null>(null);
-
 	const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
 	const [coverPreview, setCoverPreview] = useState<string | null>(null);
 
-	const { register, handleSubmit, formState: { errors } } = useForm<RegisterData>();
+	const [showPassword, setShowPassword] = useState(false);
+	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+	const { register, handleSubmit, setValue, setFocus, formState: { errors } } = useForm<RegisterFormData>({
+		resolver: zodResolver(registerUserSchema),
+		mode: 'onTouched'
+	});
 
 	const handleAvatarChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const file = e.target.files?.[0];
 
 		if (file) {
-			setAvatarFile(file);
+			setValue('avatar', file, { shouldValidate: true });
 			setAvatarPreview(URL.createObjectURL(file));
 		}
 	};
@@ -32,34 +38,26 @@ const Register = () => {
 		const file = e.target.files?.[0];
 
 		if (file) {
-			setCoverFile(file);
+			setValue('coverImage', file, { shouldValidate: true });
 			setCoverPreview(URL.createObjectURL(file));
 		}
 	};
 
-	const onSubmit: SubmitHandler<RegisterData> = (data) => {
+	const onSubmit: SubmitHandler<RegisterFormData> = (data) => {
 		const formData = new FormData();
 
+		formData.append('firstName', data.firstName.trim());
+		formData.append('lastName', data.lastName.trim());
 		formData.append('username', data.username.toLowerCase().trim());
 		formData.append('email', data.email.toLowerCase().trim());
-		formData.append('firstName', data.firstName.toLowerCase().trim());
-		formData.append('lastName', data.lastName.toLowerCase().trim());
+		formData.append('password', data.password);
+		formData.append('confirmPassword', data.confirmPassword);
 
-		if (data.password) formData.append('password', data.password);
-
-		if (avatarFile) formData.append('avatar', avatarFile);
-
-		if (coverFile) formData.append('coverImage', coverFile);
+		if (data.avatar) formData.append('avatar', data.avatar);
+		if (data.coverImage) formData.append('coverImage', data.coverImage);
 
 		registerUser(formData);
 	};
-
-	const { ref: avatarRef, ...avatarRegister } = register('avatar', {
-		required: 'Avatar image is required',
-	});
-
-	const { ref: coverRef, ...coverRegister } = register('coverImage');
-
 
 	return (
 		<>
@@ -86,26 +84,24 @@ const Register = () => {
 						</p>
 					</div>
 
-
 					<form onSubmit={handleSubmit(onSubmit)}>
-						
 						<div className='relative'>
 							<div className='relative group h-32 md:h-44 w-full bg-zinc-200 dark:bg-zinc-800 overflow-hidden'>
 								{coverPreview ? (
 									<img
 										src={coverPreview}
 										alt='Cover'
-										className='w-full h-full object-cover'
+										className='size-full object-cover'
 									/>
 								) : (
-									<div className='w-full h-full flex items-center justify-center text-zinc-400 text-sm italic'>
-										No cover image selected
+									<div className='size-full flex items-center justify-center text-zinc-400 text-sm italic'>
+										No cover image selected (Optional)
 									</div>
 								)}
-								
+
 								<label
 									htmlFor='coverImage'
-									className='absolute bottom-2 right-2 bg-black/50 hover:bg-black/70 backdrop-blur-md text-white p-2 rounded-lg cursor-pointer transition-all opacity-0 group-hover:opacity-100 z-10'
+									className='absolute bottom-2 right-2 bg-black/50 hover:bg-black/70 backdrop-blur-md text-white p-2 rounded-lg cursor-pointer transition-all opacity-80 group-hover:opacity-100 z-10'
 									title='Upload cover image'
 								>
 									<Camera size={20} />
@@ -115,24 +111,19 @@ const Register = () => {
 									id='coverImage'
 									accept='image/*'
 									className='hidden'
-									{...coverRegister}
-									onChange={(e) => {
-										coverRegister.onChange(e);
-										handleCoverChange(e);
-									}}
-									ref={coverRef}
+									onChange={handleCoverChange}
 								/>
 							</div>
-							
+
 							<div className='absolute -bottom-10 left-8 size-24 md:size-32 rounded-full border-4 border-zinc-50 dark:border-zinc-900 bg-zinc-100 dark:bg-zinc-800 overflow-hidden shadow-xl group/avatar'>
 								{avatarPreview ? (
 									<img
 										src={avatarPreview}
 										alt='Avatar'
-										className='w-full h-full object-cover'
+										className='size-full object-cover'
 									/>
 								) : (
-									<div className='w-full h-full flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-400'>
+									<div className='size-full flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-400'>
 										<User
 											size={48}
 											strokeWidth={1.5}
@@ -152,23 +143,30 @@ const Register = () => {
 									id='avatar'
 									accept='image/*'
 									className='hidden'
-									{...avatarRegister}
-									onChange={(e) => {
-										avatarRegister.onChange(e);
-										handleAvatarChange(e);
-									}}
-									ref={avatarRef}
+									onChange={handleAvatarChange}
 								/>
 							</div>
 						</div>
 
-						{errors.avatar && (
-							<p className='text-red-500 text-xs mt-12 ml-8'>
-								Please upload an avatar image to continue.
-							</p>
-						)}
+						<p className='text-right text-xs text-zinc-400 italic pl-36 pr-4 mt-2 leading-tight'>
+							Max size for avatar & cover: <span className='font-semibold'>200KB</span>
+						</p>
 
-						<div className={`p-8 ${errors.avatar ? 'pt-7' : 'pt-14'}`}>
+						<div className='mt-8 sm:mt-10 px-8 flex flex-col gap-1'>
+							{errors.avatar && (
+								<p className='text-red-500 text-xs font-medium'>
+									{errors.avatar.message}
+								</p>
+							)}
+
+							{errors.coverImage && (
+								<p className='text-red-500 text-xs font-medium'>
+									{errors.coverImage.message}
+								</p>
+							)}
+						</div>
+						
+						<div className='p-8 pt-4'>
 							<div className='mb-8'>
 								<p className='text-zinc-500 dark:text-zinc-400 mt-1'>
 									Fill in your details to get started.
@@ -176,133 +174,139 @@ const Register = () => {
 							</div>
 
 							<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-								<div className='space-y-1.5'>
-									<label
-										htmlFor='firstName'
-										className='text-sm font-medium ml-1'
-									>
-										First Name *
-									</label>
-									<input
-										type='text'
-										id='firstName'
-										placeholder='John'
-										className={`w-full p-3 rounded-xl bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 outline-none focus:ring-2 focus:ring-purple-600 transition-all ${errors.firstName ? 'focus:ring-red-500 shadow-md shadow-red-500/30' : ''}`}
-										{...register('firstName', {
-											required: 'First name is required',
-										})}
-									/>
-									{errors.firstName && (
-										<p className='text-red-500 text-xs ml-1'>
-											{errors.firstName.message}
-										</p>
-									)}
-								</div>
-
-								<div className='space-y-1.5'>
-									<label
-										htmlFor='lastName'
-										className='text-sm font-medium ml-1'
-									>
-										Last Name *
-									</label>
-									<input
-										type='text'
-										id='lastName'
-										placeholder='Doe'
-										className={`w-full p-3 rounded-xl bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 outline-none focus:ring-2 focus:ring-purple-600 transition-all ${errors.lastName ? 'focus:ring-red-500 shadow-md shadow-red-500/30' : ''}`}
-										{...register('lastName', {
-											required: 'Last name is required',
-										})}
-									/>
-									{errors.lastName && (
-										<p className='text-red-500 text-xs ml-1'>
-											{errors.lastName.message}
-										</p>
-									)}
-								</div>
-
-								<div className='space-y-1.5'>
-									<label
-										htmlFor='username'
-										className='text-sm font-medium ml-1'
-									>
-										Username *
-									</label>
-									<input
-										type='text'
-										id='username'
-										placeholder='johndoe'
-										className={`w-full p-3 rounded-xl bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 outline-none focus:ring-2 focus:ring-purple-600 transition-all ${errors.username ? 'focus:ring-red-500 shadow-md shadow-red-500/30' : ''}`}
-										{...register('username', {
-											required: 'Username is required',
-										})}
-									/>
-									{errors.username && (
-										<p className='text-red-500 text-xs ml-1'>
-											{errors.username.message}
-										</p>
-									)}
-								</div>
-
-								<div className='space-y-1.5'>
-									<label
-										htmlFor='email'
-										className='text-sm font-medium ml-1'
-									>
-										Email *
-									</label>
-									<input
-										type='email'
-										id='email'
-										placeholder='john@example.com'
-										className={`w-full p-3 rounded-xl bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 outline-none focus:ring-2 focus:ring-purple-600 transition-all ${errors.email ? 'focus:ring-red-500 shadow-md shadow-red-500/30' : ''}`}
-										{...register('email', {
-											required: 'Email is required',
-										})}
-									/>
-									{errors.email && (
-										<p className='text-red-500 text-xs ml-1'>
-											{errors.email.message}
-										</p>
-									)}
-								</div>
-							</div>
-							
-							<div className='mt-5 space-y-1.5'>
-								<label
-									htmlFor='password'
-									className='text-sm font-medium ml-1'
-								>
-									Password *
-								</label>
-								<input
-									type='password'
-									id='password'
-									placeholder='••••••••'
-									className={`w-full p-3 rounded-xl bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 outline-none focus:ring-2 focus:ring-purple-600 transition-all ${errors.password ? 'focus:ring-red-500 shadow-md shadow-red-500/30' : ''}`}
-									{...register('password', {
-										required: 'Password is required',
-										minLength: {
-											value: 6,
-											message: 'At least 6 characters required',
-										},
-									})}
+								<Input 
+									label='First Name'
+									type='text'
+									placeholder='John'
+									title='First Name'
+									leftIcon={
+										<User 
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('firstName')}
+										/>
+									}
+									error={errors.firstName?.message}
+									isOptionalField={false}
+									{...register('firstName')}
 								/>
-								{errors.password && (
-									<p className='text-red-500 text-xs ml-1'>
-										{errors.password.message}
-									</p>
-								)}
+								
+								<Input 
+									label='Last Name'
+									type='text'
+									placeholder='Doe'
+									title='Last Name'
+									leftIcon={
+										<User 
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('lastName')}
+										/>
+									}
+									error={errors.lastName?.message}
+									isOptionalField={false}
+									{...register('lastName')}
+								/>
+
+								<Input 
+									label='Username'
+									type='text'
+									placeholder='johndoe'
+									title='Username'
+									leftIcon={
+										<AtSign 
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('username')}
+										/>
+									}
+									error={errors.username?.message}
+									isOptionalField={false}
+									{...register('username')}
+								/>
+
+								<Input 
+									label='Email'
+									type='email'
+									placeholder='john@example.com'
+									title='Email'
+									leftIcon={
+										<Mail 
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('email')}
+										/>
+									}
+									error={errors.email?.message}
+									isOptionalField={false}
+									{...register('email')}
+								/>
 							</div>
 
-							<button
+							<div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-5'>
+								<Input
+									label='Password'
+									type={showPassword ? 'text' : 'password'}
+									placeholder='••••••••'
+									title='Password'
+									leftIcon={
+										<Lock
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('password')}
+										/>
+									}
+									rightElement={
+										<button
+											type='button'
+											onClick={() => setShowPassword(!showPassword)}
+											className='text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors cursor-pointer'
+											title={showPassword ? 'Hide' : 'Show'}
+										>
+											{showPassword ? <EyeOff size={18} /> : <Eye size={18}/>}
+										</button>
+									}
+									error={errors.password?.message}
+									isOptionalField={false}
+									{...register('password')}
+								/>
+
+								<Input
+									label='Confirm Password'
+									type={showConfirmPassword ? 'text' : 'password'}
+									placeholder='••••••••'
+									title='Confirm Password'
+									leftIcon={
+										<Lock
+											className='size-4 cursor-pointer'
+											onClick={() => setFocus('confirmPassword')}
+										/>
+									}
+									rightElement={
+										<button
+											type='button'
+											onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+											className='text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors cursor-pointer'
+											title={showConfirmPassword ? 'Hide' : 'Show'}
+										>
+											{showConfirmPassword ? <EyeOff size={18} /> : <Eye size={18}/>}
+										</button>
+									}
+									error={errors.confirmPassword?.message}
+									isOptionalField={false}
+									{...register('confirmPassword')}
+								/>
+							</div>
+
+							<Button 
 								type='submit'
 								disabled={isPending}
-								className={`w-full mt-8 py-4 bg-purple-600 hover:bg-purple-700 text-white font-bold rounded-2xl transition-all active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed shadow-lg shadow-purple-500/20 cursor-pointer ${isPending ? 'active:scale-none' : ''}`}
+								title={isPending ? 'Processing Registration...' : 'Create Account'}
+								className={`mt-8 mb-5 ${isPending ? 'active:scale-none' : ''}`}
 							>
-								{isPending ? 'Processing Registration...' : 'Create Account'}
-							</button>
+								{isPending ? (
+									<>
+										<Loader2 className='animate-spin' size={18} />
+									</>
+								) : (
+									'Create Account'
+								)}
+							</Button>
 
 							<div className='mt-8 pt-6 border-t border-zinc-200 dark:border-zinc-800 text-center'>
 								<p className='text-sm text-zinc-500'>
@@ -316,7 +320,6 @@ const Register = () => {
 								</p>
 							</div>
 						</div>
-
 					</form>
 				</div>
 			</section>
