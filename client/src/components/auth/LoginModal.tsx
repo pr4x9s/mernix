bd7932b7504edd77a035d8245b67a4f489ec20da
pr4x9s <9tbs6@proton.me>
2026-08-21 22:54:38 +0530

    test push
    Signed-off-by: `pr4x9s` <9tbs6@proton.me>

--- client/src/components/auth/LoginModal.tsx
+++ client/src/components/auth/LoginModal.tsx
@@ -28,7 +28,7 @@ const LoginModal = () => {
 				<button
 					onClick={() => setLoginModalOpen(false)}
 					className='absolute right-5 top-5 p-2 hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-200 transition-all disabled:opacity-50 rounded-full cursor-pointer active:scale-[0.90] group'
-					title='Close'
+					title='Close Now'
 				>
 					<X size={20} className='group-hover:rotate-90 transition ease-in duration-300' />
 				</button>
