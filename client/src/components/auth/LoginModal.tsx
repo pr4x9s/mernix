0caa1f976f0c43e89e060d7f42ad8595c0952bdc
pr4x9s <9tbs6@proton.me>
2026-08-22 19:12:06 +0530

    client/refactor: add button types to `LoginModal` and `ConfirmationModal` for accessibility
    Signed-off-by: `pr4x9s` <9tbs6@proton.me>

--- client/src/components/auth/LoginModal.tsx
+++ client/src/components/auth/LoginModal.tsx
@@ -26,6 +26,7 @@ const LoginModal = () => {
 			{/* modal box */}
 			<div className='relative w-full max-w-sm bg-white dark:bg-zinc-900 rounded-4xl shadow-2xl overflow-hidden p-8 text-center border border-zinc-200 dark:border-zinc-800'>
 				<button
+					type='button'
 					onClick={() => setLoginModalOpen(false)}
 					className='absolute right-5 top-5 p-2 hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-200 transition-all disabled:opacity-50 rounded-full cursor-pointer active:scale-[0.90] group'
 					title='Close'
@@ -47,12 +48,13 @@ const LoginModal = () => {
 
 				<div className='flex flex-col gap-3'>
 					<Button
+						type='button'
 						onClick={handleGoToFullLogin}
-						className=''
 					>
 						Sign in
 					</Button>
 					<Button
+						type='button'
 						onClick={() => setLoginModalOpen(false)}
 						className='bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700 text-zinc-900 dark:text-white shadow-zinc-500/20'
 					>
