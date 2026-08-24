623aa1077f561c85a16ca85428bc651c5a85f1b8
pr4x9s <9tbs6@proton.me>
2026-08-24 19:43:33 +0530

    client/refactor: add dynamic theme support to `Toaster` component in `App.tsx`
    Signed-off-by: `pr4x9s` <9tbs6@proton.me>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
 
     return (
         <div className='min-h-screen transition-colors duration-300 bg-white dark:bg-zinc-950 text-zinc-900 dark:text-white'>
-            <Toaster richColors expand={false} duration={3000} />
+            <Toaster richColors expand={false} duration={3000} theme={isDarkMode ? 'dark' : 'light'} />
 
             <LoginModal />
 
